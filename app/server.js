02b7e36fa3110064dbe263e45ba1e1d9a4fdd179
LeonidChetverikov <34073185+LeonidChetverikov@users.noreply.github.com>
2020-04-27 22:40:39 +0300

    Update server.js

--- app/server.js
+++ app/server.js
@@ -1,4 +1,4 @@
-'use strict';
+u'use strict';
 
 const express = require('express');
 
@@ -7,7 +7,7 @@ const HOST = '0.0.0.0';
 
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello world\n');
+  res.send('Hello meetup\n');
 });
 
 app.listen(PORT, HOST);
